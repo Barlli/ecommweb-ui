8e52958f26164a5c075d5c8a07f2d057ba881c2b
Barlli <56704426+Barlli@users.noreply.github.com>
2023-06-06 21:20:59 +0200

    fixing currency

--- src/app/buy-product/buy-product.component.ts
+++ src/app/buy-product/buy-product.component.ts
@@ -120,7 +120,7 @@ export class BuyProductComponent implements OnInit {
       key: response.key,
       amount: response.amount,
       currency: response.currency,
-      name: 'Learn programming yourself',
+      name: 'Payment',
       description: 'Payment of online shopping',
       image: 'https://cdn.pixabay.com/photo/2023/01/22/13/46/swans-7736415_640.jpg',
       handler: (response: any) => {
